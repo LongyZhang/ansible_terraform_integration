49f6f971a15b3848c8dc6678433816b0d875ca5c
longy zhang <longhanzhang999@gmail.com>
2025-07-26 16:36:33 +1000

    Installed the ansible by user ec2-user , other wise the AWS will use root user to install it

--- user_data/user_data.sh
+++ user_data/user_data.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 echo "Running user data..." > /home/ec2-user/user_data.log
-yum install -y vim ansible >> /home/ec2-user/user_data.log 2>&1
-echo "Finished user data." >> /home/ec2-user/user_data.log
+yum install -y vim  >> /home/ec2-user/user_data.log 
+sudo yum install -y python3 >> /home/ec2-user/user_data.log 
+sudo -u ec2-user pip3 install --user ansible
+echo "User data completed." >> /home/ec2-user/user_data.log
+echo "Ansible version:" >> /home/ec2-user/user_data.log
+ansible --version >> /home/ec2-user/user_data.log
